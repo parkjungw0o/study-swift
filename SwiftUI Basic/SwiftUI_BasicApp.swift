45d3d14fbe107e34974ba9d592b26165806ba75b
hotcat0421 <musicboxy11@gmail.com>
2024-06-19 21:15:18 +0900

    Initial Commit

---
+++ SwiftUI Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI Basic
+//
+//  Created by 박정우 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
